197fc8d05d190f04376c6d04199d04712079a2bd
Logan Zechella <jloganzechella@gmail.com>
2022-09-13 17:49:53 -0400

    WORK IN PROGRESS Yes/no buttons JS basic logic functioning to move through prompt questions

--- scripts/app.js
+++ scripts/app.js
@@ -2,30 +2,39 @@ let startButton = document.getElementById('btn-start');
 let promptsH2 = document.getElementById('title');
 let promptsP = document.getElementById('description');
 let buttonsDiv = document.getElementById('buttons');
+let yesButton = document.getElementById('btn-yes');
+let noButton = document.getElementById('btn-no');
+
+let question1 = 'Is this your first time attending a Venture Connectors Event?';
+let question2 = 'Question 2';
+let question3 = 'Question 3';
+let question4 = 'Question 4';
+
+let questionArray = [question1, question2, question3, question4];
 
 startButton.addEventListener('click', function() {
     startButton.style.display = 'none';
     promptsP.style.display = 'none';
-    promptsH2.innerHTML = 'Is this your first time attending a Venture Connectors Event?';
+    promptsH2.innerHTML = questionArray[0];
     
-    if (startButton.style.display === 'none') {
-        let yesButton = document.createElement('button');
-        yesButton.innerHTML = 'Yes';
-        yesButton.setAttribute('id', 'btn-yes');
-        buttonsDiv.appendChild(yesButton);
-        let noButton = document.createElement('button');
-        noButton.innerHTML = 'No';
-        noButton.setAttribute('id', 'btn-no');
-        buttonsDiv.appendChild(noButton);
-    }
+    yesButton.style.display = 'block';
+    noButton.style.display = 'block';
 });
 
-let yesButton = document.getElementById('btn-yes');
-let noButton = document.getElementById('btn-no');
-let [yesCount, noCount] = 0;
+
+let yesCount = 0;
+let noCount = 0;
 
 function yesButtonClick() {
     yesCount++;
-    yesButton.style.backgroundColor = 'green';
-    yesButton.style.backgroundColor = '#2182BF';
+    let arrayIndex = yesCount + noCount;
+    promptsH2.innerHTML = questionArray[arrayIndex];
+    console.log(`Question ${arrayIndex} answered yes. Yes count: ${yesCount}, No count: ${noCount}`);
+}
+
+function noButtonClick() {
+    noCount++;
+    let arrayIndex = yesCount + noCount;
+    promptsH2.innerHTML = questionArray[arrayIndex];
+    console.log(`Question ${arrayIndex} answered no. No count: ${noCount}, Yes count: ${yesCount}`);
 }
